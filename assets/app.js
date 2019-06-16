98519c0cfd789c6752d0e5958bfea64af8ff71d5
Stephanie Mattern <smattern416@gmail.com>
2019-06-16 09:46:53 -0400

    firebase snippet

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 
-    
+    var firebaseConfig = {
+        apiKey: "AIzaSyDol1Twh0SLkBzoeWj5vk9VIFAUMk5kKqU",
+        authDomain: "train-scheduler-aaa14.firebaseapp.com",
+        databaseURL: "https://train-scheduler-aaa14.firebaseio.com",
+        projectId: "train-scheduler-aaa14",
+        storageBucket: "train-scheduler-aaa14.appspot.com",
+        messagingSenderId: "973780121242",
+        appId: "1:973780121242:web:1381d1bd99ff7f2f"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
 })
